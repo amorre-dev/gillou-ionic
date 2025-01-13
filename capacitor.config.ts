ff8035b4ecb98360f2b61cac76414f52f8f04f26
amorre <amorre@am-e.dev>
2025-01-13 15:41:20 +0100

    add double click functionnality

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'am.e.dev.gillou',
-  appName: 'gillou-ionic',
+  appName: 'Gillou',
   webDir: 'www/browser',
 };
 
--- src/app/components/music-display/music-display.component.ts
+++ src/app/components/music-display/music-display.component.ts
@@ -9,12 +9,13 @@ import {
   OnDestroy,
   effect,
 } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { Platform, GestureController } from '@ionic/angular';
 import { ScreenOrientation } from '@capacitor/screen-orientation';
 import { Score } from '../../types/score.type';
 import { ThemeService } from '../../services/theme.service';
 import { ScoreRenderedCacheService } from 'src/app/services/score-rendered-cache.service';
 import { generateCacheKey } from 'src/app/helpers/cache-key.helper';
+import { ScoreHistoryService } from 'src/app/services/score-history.service';
 
 @Component({
   selector: 'app-music-display',
@@ -30,10 +31,15 @@ export class MusicDisplayComponent
 
   isDarkMode = this.themeService.isDarkMode;
 
+  private lastOnStart: number = 0;
+  private DOUBLE_CLICK_THRESHOLD: number = 500;
+
   constructor(
     private themeService: ThemeService,
     private platform: Platform,
-    private scoreCacheService: ScoreRenderedCacheService
+    private scoreCacheService: ScoreRenderedCacheService,
+    private gestureCtrl: GestureController,
+    private historyService: ScoreHistoryService
   ) {
     ScreenOrientation.addListener('screenOrientationChange', () => {
       this.renderOrUseCache(); // Retrigger rendering when orientation changes
@@ -45,6 +51,29 @@ export class MusicDisplayComponent
 
   ngAfterViewInit() {
     this.renderOrUseCache();
+    const gesture = this.gestureCtrl.create({
+      el: this.musicCanvas.nativeElement,
+      threshold: 0,
+      onStart: () => this.onStart(),
+      gestureName: 'double-click',
+    });
+
+    gesture.enable();
+  }
+
+  private onStart() {
+    const now = Date.now();
+
+    if (Math.abs(now - this.lastOnStart) <= this.DOUBLE_CLICK_THRESHOLD) {
+      this.historyService.addToHistory(
+        this.score.id,
+        this.score.title,
+        this.score.number
+      );
+      this.lastOnStart = 0;
+    } else {
+      this.lastOnStart = now;
+    }
   }
 
   ngOnChanges(changes: SimpleChanges) {
